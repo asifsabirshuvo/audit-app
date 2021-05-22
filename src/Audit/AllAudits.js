37ad9d5afd6a47e667583cd625fef1751f3f881c
asif <asifsabirshuvo@gmail.com>
2021-05-22 16:26:51 +0600

    added shimmer skeleton

--- src/Audit/AllAudits.js
+++ src/Audit/AllAudits.js
@@ -1,24 +1,23 @@
 import Skeleton from "react-loading-skeleton";
 import React from "react";
 import { useState } from "react";
-import AuditItem from './../Components/AuditItem';
-import {Link, useHistory } from "react-router-dom";
-import Button from '@material-ui/core/Button';
+import AuditItem from "./../Components/AuditItem";
+import { Link, useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 import axios from "axios";
 
 function AllAudits() {
 	const [data, setData] = useState([]);
 	const [loader, setLoader] = useState(true);
-    const history = useHistory();
+	const history = useHistory();
 
-    console.log("hi");
+	console.log("hi");
 	//self invoing function before render is complete
 	React.useEffect(() => {
-		tryInfo();
+		renderList();
 	}, []);
 
-
-	async function tryInfo() {
+	async function renderList() {
 		// POST request using fetch with async/await
 		const requestOptions = {
 			method: "GET",
@@ -33,78 +32,124 @@ function AllAudits() {
 				"http://localhost:4000/api/v1/audit?page=1&limit=10",
 				requestOptions
 			);
-            const data = await response.json();
+			setLoader(false);
+			const data = await response.json();
 			setData(data.message);
 		} catch (err) {
-            setLoader(false);
+			setLoader(false);
 		}
 	}
-      const edit = (code) => {
-		history.push("/edit-audit/"+code);
+	const edit = (code) => {
+		history.push("/edit-audit/" + code);
+	};
 
-      };
-
-const del = async (code) => {
-
-
-	//show loader now
-	try {
-		const response = await axios.delete('http://localhost:4000/api/v1/audit/'+code);
-		if(response.data.success){
-			history.go(0)		}
-	} catch (err) {
-		setLoader(false);
-	}
-		
-};
-
+	const del = async (code) => {
+		//show loader now
+		try {
+			const response = await axios.delete(
+				"http://localhost:4000/api/v1/audit/" + code
+			);
+			if (response.data.success) {
+				history.go(0);
+			}
+		} catch (err) {
+			setLoader(false);
+		}
+	};
 
 	return (
-		<div Style="margin:auto; text-align:center; width:80%; justifyContent:center; alignItems:center; ">
-			<br></br>
-            <Link  to="/add-audit" Style="color:gray;text-decoration:none;">
-            <div Style="display:inline-block;"></div>
-            <span Style="font-size:19px;">Want to make an entry? &nbsp;&nbsp;</span>
-            <Button variant="contained" color="primary">
-                Click Here
-            </Button>
-            </Link>
+		<div>
+			{loader === false ? (
+				<div Style="margin:auto; text-align:center; width:80%; justifyContent:center; alignItems:center; ">
+					<br></br>
+					<Link
+						to="/add-audit"
+						Style="color:gray;text-decoration:none;"
+					>
+						<div Style="display:inline-block;"></div>
+						<span Style="font-size:19px;">
+							Want to make an entry? &nbsp;&nbsp;
+						</span>
+						<Button variant="contained" color="primary">
+							Click Here
+						</Button>
+					</Link>
 
-            	{data.map((item)=> {
-					 return <AuditItem
-                     code={item.code}
-                     name={item.name}
-                     address={item.address}
-                     description={item.description}
-                     latitude={item.latitude}
-                     longitude={item.longitude}
-                     createdAt={item.createdAt}
-                     updatedAt={item.updatedAt}
-                     createdBy={item.createdBy}
-                     updatedBy={item.updatedBy}
-                     edit = {edit}
-                     del = {del}
-                   />
-				})}
-			{/* <Skeleton width={300} />
-			<br></br>
-			<div Style="display: inline-block;">
-				<Skeleton height={60} width={60} />
-				<span Style="margin-left:10px;"></span>
-				<Skeleton height={60} width={60} />
-				<span Style="margin-left:10px;"></span>
-				<Skeleton height={60} width={60} />
-				<span Style="margin-left:10px;"></span>
-				<Skeleton height={60} width={60} />
-				<span Style="margin-left:10px;"></span>
-				<Skeleton height={60} width={60} />
-			</div>
-			<Skeleton count={3} />
-			<div Style="display: inline-block;">
-				<Skeleton height={35} width={35} />
-				<span Style="margin-left:10px;"></span>
-				<Skeleton height={35} width={35} />
-			</div> */}
+					{data.map((item) => {
+						return (
+							<AuditItem
+								code={item.code}
+								name={item.name}
+								address={item.address}
+								description={item.description}
+								latitude={item.latitude}
+								longitude={item.longitude}
+								createdAt={item.createdAt}
+								updatedAt={item.updatedAt}
+								createdBy={item.createdBy}
+								updatedBy={item.updatedBy}
+								edit={edit}
+								del={del}
+							/>
+						);
+					})}
+				</div>
+			) : (
+				<div Style="margin:auto; text-align:left; width:120vh; margin-top:10vh; justifyContent:left; alignItems:left; ">
+					<Skeleton width={350} />
+					<br></br>
+					<Skeleton width={200} />
+					<br></br>
+					<Skeleton width={900}  />{" "}
+					<br></br>
+					<Skeleton width={900}  />{" "}
+					<br></br>
+					<Skeleton width={900}  />{" "}
+					<br></br>
+					<div Style="display: inline-block;">
+						{" "}
+						<Skeleton height={35} width={75} />{" "}
+						<span Style="margin-left:10px;"></span>{" "}
+						<Skeleton height={35} width={75} />{" "}
+					</div>
+					<br></br><br></br><br></br>
+
+					<Skeleton width={350} />
+					<br></br>
+					<Skeleton width={200} />
+					<br></br>
+					<Skeleton width={900}  />{" "}
+					<br></br>
+					<Skeleton width={900}  />{" "}
+					<br></br>
+					<Skeleton width={900}  />{" "}
+					<br></br>
+					<div Style="display: inline-block;">
+						{" "}
+						<Skeleton height={35} width={75} />{" "}
+						<span Style="margin-left:10px;"></span>{" "}
+						<Skeleton height={35} width={75} />{" "}
+					</div>
+					<br></br><br></br><br></br>
+
+					<Skeleton width={350} />
+					<br></br>
+					<Skeleton width={200} />
+					<br></br>
+					<Skeleton width={900}  />{" "}
+					<br></br>
+					<Skeleton width={900}  />{" "}
+					<br></br>
+					<Skeleton width={900}  />{" "}
+					<br></br>
+					<div Style="display: inline-block;">
+						{" "}
+						<Skeleton height={35} width={75} />{" "}
+						<span Style="margin-left:10px;"></span>{" "}
+						<Skeleton height={35} width={75} />{" "}
+					</div>
+				</div>
+			)}
 		</div>
 	);
 }
